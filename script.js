9fa60098d803aff573c4f249ae3c6ab873fd1695
btramel <bradtramel@gmail.com>
2022-02-17 18:03:51 -0600

    all logic complete.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const gameBoard = (() => {
     'use strict';
+
 })();
 
 const display = (() => {
@@ -7,4 +8,110 @@ const display = (() => {
 
 })();
 
-const playerSelect = ;
+const playerSelect = () => {
+    
+};
+
+// get display for messages
+// get cells to click inside of and hover over 
+// get button for reset
+
+// assign cells a number inside an array 0 - 8
+
+// switch between x's and o's each turn
+// check for win after each turn
+
+
+const X_CLASS = 'x'
+const O_CLASS = 'o'
+const WINNING_COMBINATIONS = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+]
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+const winningMessageElement = document.getElementById('winning-message')
+const restartButton = document.getElementById('restartButton')
+let circleTurn
+
+startGame()
+
+restartButton.addEventListener('click', startGame)
+
+function startGame () {
+    circleTurn = false
+    cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS)
+        cell.classList.remove(O_CLASS)
+        cell.removeEventListener('click', handleClick)
+        cell.addEventListener('click', handleClick, { once: true }) // this ensures it only fires once
+    })
+    setBoardHoverClass()
+    winningMessageElement.classList.remove('show')
+
+}
+
+function handleClick(e) {
+    const cell = e.target
+    const currentClass = circleTurn ? O_CLASS : X_CLASS
+    placeMark(cell, currentClass)
+    if ( checkWin(currentClass) ) {
+        endGame(false)
+    } else if (isDraw()) {
+        endGame(true)
+    } else {
+        swapTurn()
+        setBoardHoverClass()
+    }
+}
+
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) ||
+        cell.classList.contains(O_CLASS)
+    }
+    )}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+}
+
+function swapTurn() {
+    circleTurn = !circleTurn
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS)
+    board.classList.remove(O_CLASS)
+    if (circleTurn) {
+        board.classList.add(O_CLASS)
+    }
+    else { 
+        board.classList.add(X_CLASS) 
+    }
+}
+
+function checkWin(currentClass) {
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains
+            (currentClass)
+        })
+    })
+}
+
+function endGame(draw) {
+    if (draw) {
+        winningMessageTextElement.innerText = 'Draw!'
+    } else {
+        winningMessageTextElement.innerText = `${circleTurn ? "O" : "X"} Wins!`
+    }
+    winningMessageElement.classList.add('show')
+}
